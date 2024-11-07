89178b3f858afe6726ac7851a5ed9cef8991de61
syed-umar-0311 <umarzain2005@gmail.com>
2024-11-07 22:32:26 +0500

    finish task

--- src/components/pages/Navigation/Navigation.js
+++ src/components/pages/Navigation/Navigation.js
@@ -1,31 +1,26 @@
-import React from 'react';
-import '../Navigation/Navigation.css';
-import Menu from '../../../images/Menu.png';
-import vector from '../../../images/Vector.png';
-import navlogo from '../../../images/navlogo.png'
+import React from "react";
+import "../Navigation/Navigation.css";
+import Menu from "../../../images/Menu.png";
+import vector from "../../../images/Vector.png";
+import navlogo from "../../../images/navlogo.png";
+import { Link } from "react-router-dom";
 import { CiSquareCheck } from "react-icons/ci";
 import { GoHome } from "react-icons/go";
 import { RiShoppingBag3Line } from "react-icons/ri";
 import { FiUser } from "react-icons/fi";
 import { BsFilterLeft } from "react-icons/bs";
 
-
-
-
-
-
-
-
 function Navigation() {
   return (
-    <div className='main'>
+    <div className="main">
       <div className="app">
         {/* Header */}
         <header className="header">
-          <img src={navlogo} alt="Logo" className="logo" /> {/* Add the image */}
+          <img src={navlogo} alt="Logo" className="logo" />{" "}
+          {/* Add the image */}
           <div className="icons">
-            <img src={vector} alt='bell' className="icon" />
-            <img src={Menu} alt='nav' className="icon" />
+            <img src={vector} alt="bell" className="icon" />
+            <img src={Menu} alt="nav" className="icon" />
           </div>
         </header>
 
@@ -36,11 +31,24 @@ function Navigation() {
 
         {/* Bottom Navigation */}
         <footer className="bottom-nav">
-          <BsFilterLeft className="nav-icon" />
-          <CiSquareCheck className="nav-icon " />
-          <GoHome className="nav-icon" />
-          <RiShoppingBag3Line className="nav-icon" />
-          <FiUser className="nav-icon" />
+          <Link to="/filter">
+            <BsFilterLeft className="nav-icon" />
+          </Link>
+          <Link to="/check">
+            {" "}
+            <CiSquareCheck className="nav-icon " />
+          </Link>
+          <Link to="/home">
+            {" "}
+            <GoHome className="nav-icon" />
+          </Link>
+          <Link to="/cart">
+            {" "}
+            <RiShoppingBag3Line className="nav-icon" />
+          </Link>
+          <Link to="/login">
+            <FiUser className="nav-icon" />
+          </Link>
         </footer>
       </div>
     </div>
